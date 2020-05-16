6e078208ac3b514015d8bd976e71724c6c78f531
VaderSteve76 <alverson.s@yahoo.com>
2020-05-16 18:51:45 -0400

    Added prevBtn & nextBtn Event Listeners

--- script.js
+++ script.js
@@ -9,3 +9,28 @@ const answerEl = document.getElementById('answer');
 const addCardBtn = document.getElementById('add-card');
 const clearBtn = document.getElementById('clear');
 const addContainer = document.getElementById('add-container');
+
+
+// Event Listeners
+
+// Next button
+nextBtn.addEventListener('click', () => {
+  cardsEl[currentActiveCard].className = 'card left';
+  currentActiveCard = currentActiveCard + 1;
+  if(currentActiveCard > cardsEl.length - 1) {
+    currentActiveCard = cardsEl.length - 1;
+  }
+  cardsEl[currentActiveCard].className = 'card active';
+  updateCurrentText();
+});
+
+// Prev button
+prevBtn.addEventListener('click', ()=> {
+  cardsEl[currentActiveCard].className = 'card right';
+  currentActiveCard = currentActiveCard - 1;
+  if(currentActiveCard < 0) {
+    currentActiveCard = 0;
+  }
+  cardsEl[currentActiveCard].className = 'card active';
+  updateCurrentText();
+});
